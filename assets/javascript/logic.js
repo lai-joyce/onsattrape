76dfc7deeec6e27b1ff278d88ee0748e0e79f979
halvorson <michael.t.halvorson@gmail.com>
2017-06-12 19:48:18 -0700

    setup page is working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,3 +5,13 @@ var testContact = {
 	phone: 6514429511,
 	email: "michael.t.halvorson@gmail.com"
 }
+
+var database = firebase.database();
+var firebaseRef = database.ref();
+
+firebaseRef.on("value", function(snap) {
+	snap.forEach(function(contact) {
+		var contactVal = contact.val();
+		$("#contactTable").append($("<tr>").data('key',contactVal.key).append($("<td>").text(contactVal.name)).append($("<td>").text(contactVal.telephone)).append($("<td>").text(contactVal.email)).append($("<td>").text(contactVal.days)).append($("<td>")).append($("<td>")).append($("<td>").append($("<button>").text("Edit").addClass("edit btn btn-default"))))
+	});
+});
