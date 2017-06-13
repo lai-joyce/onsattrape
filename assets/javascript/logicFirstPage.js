5c1851111228b2496a2ecb9ed51384d056100bda
halvorson <michael.t.halvorson@gmail.com>
2017-06-13 08:42:12 -0700

    added filler function for selectionLogic

--- assets/javascript/logicFirstPage.js
+++ assets/javascript/logicFirstPage.js
@@ -1,12 +1,12 @@
 alert("working");
 var config = {
-			apiKey: "AIzaSyDp8OOm78pBLJEKkE6r4JKBg_MPzqhiCVQ",
-			authDomain: "onsattrape.firebaseapp.com",
-			databaseURL: "https://onsattrape.firebaseio.com",
-			projectId: "onsattrape",
-			storageBucket: "onsattrape.appspot.com",
-			messagingSenderId: "1094671185695"
-		};
+	apiKey: "AIzaSyDp8OOm78pBLJEKkE6r4JKBg_MPzqhiCVQ",
+	authDomain: "onsattrape.firebaseapp.com",
+	databaseURL: "https://onsattrape.firebaseio.com",
+	projectId: "onsattrape",
+	storageBucket: "onsattrape.appspot.com",
+	messagingSenderId: "1094671185695"
+};
 firebase.initializeApp(config);
 
 var uid = "-KmUZTaohwghpuagw";
@@ -18,5 +18,12 @@ var database = firebase.database();
 var contactsRef = database.ref('users/'+uid+'/contacts');
 
 contactsRef.on("child_added", function(snapshot) {
-	console.log(snapshot.val());
+	if (selectionLogic(snapshot.val())) {
+		console.log(snapshot.val());
+	}
 });
+
+function selectionLogic(snapValues) {
+	//This is where the magic happens; this function should return true or false depending on whether someone was randomly selected, whether it's their birthday, etc.
+	return true;
+}
