26946780246b79baacfb2af0d92758d437880eb6
halvorson <michael.t.halvorson@gmail.com>
2017-06-15 08:27:40 -0700

    got contact parsing working, need to build selector

--- assets/javascript/authlogic.js
+++ assets/javascript/authlogic.js
@@ -4,15 +4,15 @@ var database = firebase.database();
 var contactsRef = database.ref('users/'+uid+'/contacts');
 
 var provider = new firebase.auth.GoogleAuthProvider();
-//provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
 
-console.log("help!");
+var user;
+var token;
 
 function login() { 
 	firebase.auth().signInWithPopup(provider).then(function(result) {
-		var token = result.credential.accessToken;
-		var user = result.user;
-		createNewUser(user);
+		token = result.credential.accessToken;
+		user = result.user;
+		checkIfNewUser(user);
 		console.log(user);
 	}).catch(function(error) {
 		var errorCode = error.code;
@@ -23,20 +23,56 @@ function login() {
 }
 
 function createNewUser(user) {
-	if(checkIfUserExists(user)) {
-		console.log(database.ref('users/'+user.uid));
-	} else {
-		console.log('trying to write to firebase');
-		database.ref('users/'+user.uid+'/info').set({
-			name: user.displayName,
-			email: user.email
-		});
-	}
+	console.log('trying to write to firebase');
+	database.ref('users/'+user.uid+'/info').set({
+		name: user.displayName,
+		email: user.email
+	});
+	//window.location.href = 'contacts.html';
 }
 
-function checkIfUserExists(user) {
+function checkIfNewUser(user) {
 	var usersRef = database.ref('users');
 	usersRef.child(user.uid).once('value', function(snapshot) {
-		return (snapshot.val() !== null);
+		if (snapshot.val() === null) {
+			createNewUser(user);
+		} else {
+			console.log("Welcome back");
+			//window.location.href = 'index.html';
+			return false;
+		}
 	});
 }
+
+function askForContactPermission() {
+	provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+	provider.addScope('https://www.google.com/m8/feeds/');
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+		token = result.credential.accessToken;
+		user = result.user;
+		grabContacts();
+	}).catch(function(error) {
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		var email = error.email;
+		var credential = error.credential;
+	});
+}
+
+function grabContacts() {
+	$.ajax({ 
+		url: "https://www.google.com/m8/feeds/contacts/" + user.email +"/full?access_token=" + token + "&alt=json", 
+		dataType: "JSONP", 
+		success: function(res){ 
+			console.log(res); 
+		}, 
+		error: function(xhr,status,error){
+			console.log(xhr);
+			console.log(status);
+			console.log(error);
+		}
+	});
+}
+
+$(document).on("click", "#loginButton", login);
+$(document).on("click", "#getContacts", askForContactPermission);
