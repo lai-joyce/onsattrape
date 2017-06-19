1eed52939a44f92350101b951779c9a26045b788
Slobodan Krstic <slobodankrstic@Slobodans-MacBook-Pro.local>
2017-06-19 10:58:12 -0700

    Buttons added

--- assets/javascript/panels.js
+++ assets/javascript/panels.js
@@ -401,31 +401,31 @@ function createNewModal() {
 
 
 
-// Get the modal
-var modal = document.getElementById('#myModal');
+// // Get the modal
+// var modal = document.getElementById('#myModal');
 
-// Get the button that opens the modal
-var btn = $(".viewNotes");
+// // Get the button that opens the modal
+// var btn = $(".viewNotes");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-//modal.addNotesCSS.display = "block";
+// //modal.addNotesCSS.display = "block";
 
-// When the user clicks on the button, open the modal 
-$(document).on("click", ".viewNotes", function() {
-    modal.addNotesCSS.display = "block";
-});
+// // When the user clicks on the button, open the modal 
+// $(document).on("click", ".viewNotes", function() {
+//     modal.addNotesCSS.display = "block";
+// });
 
-//When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.addNotesCSS.display = "none";
-}
+// //When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.addNotesCSS.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.addNotesCSS.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.addNotesCSS.display = "none";
+//     }
+// }
 
