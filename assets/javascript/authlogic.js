fa461beddda9947ff3d30ca921f4e013f5ec8509
halvorson <michael.t.halvorson@gmail.com>
2017-07-09 15:58:13 -0700

    changed login to redirect from popup

--- assets/javascript/authlogic.js
+++ assets/javascript/authlogic.js
@@ -8,23 +8,35 @@ $(function() {
 
 	//Logs people in in popup, then checks if they exist
 	function login() { 
-		firebase.auth().signInWithPopup(provider).then(function(result) {
-			token = result.credential.accessToken;
-			user = result.user;
-			checkIfNewUser(user);
-			console.log(user);
+		firebase.auth().signInWithRedirect(provider);
+		firebase.auth().getRedirectResult().then(function(result) {
+			console.log("this is being triggered");
+			if (result.credential) {
+				console.log("there is a credential");
+				token = result.credential.accessToken;
+				user = result.user;
+			}
 		}).catch(function(error) {
 			var errorCode = error.code;
 			var errorMessage = error.message;
 			var email = error.email;
 			var credential = error.credential;
+			console.log(error);
 		});
 	}
 
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			checkIfNewUser(user);
+			console.log(user);
+		}
+	});
+
 	//Checks if user exists, and redirects to home page, otherwise creates new user
 	function checkIfNewUser(user) {
-		var usersRef = database.ref('users');
-		usersRef.child(user.uid).once('value', function(snapshot) {
+		var userRef = database.ref('users/' + user.uid);
+		console.log("checking for user: " + user.uid);
+		userRef.once('value', function(snapshot) {
 			if (snapshot.val() === null) {
 				createNewUser(user);
 			} else {
